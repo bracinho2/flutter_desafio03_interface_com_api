3d2a9ce2b9b69dd523b49a4d6aa7640faabb27c2
bracinho2 <bracinho2@hotmail.com>
2022-10-19 21:49:33 -0300

    Atualização do Tratamento de Erros

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,32 @@ void main() {
     ),
   );
 }
+
+
+//Modular
+//O que ele resolve?
+//1. As injeçoes; (Contratos (classes abstratas + injeção))
+//2. Rotas de navegação
+
+//MVC + Repository
+//view > tela;
+//controller > conversa com a tela e com a MODEL e com o repository;
+//Repository > converter coisas que vem da API para seu modelo
+//Datasource > conversar com a API e enviar as coisas da API para o Repository;
+
+/*
+
+API > Datasource > Repository > Model > controller > tela;
+
+Datasource > qual sua funçao?
+1. conversar com HttpClient;
+2. conhece o caminho dos dados
+
+Httpclient
+1. Sabe buscar;
+
+
+*/
+
+//Ciclo > tela chama o Controller > controller chama o Respository (aqui quando voltar dados chama o model) > repository chama o datasource que fala coma api.
+
